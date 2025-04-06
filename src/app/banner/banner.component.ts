ac7fafc0e6efc4fb9917f5e61b3be248df5f8169
phi1ip83 <philip.dossantos@hotmail.com>
2025-04-06 11:38:11 -0400

    creating banner, fixing styling to align with customer demands

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+@Component({
+  selector: 'app-banner',
+  imports: [RouterLink],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.css'
+})
+export class BannerComponent {
+
+}
