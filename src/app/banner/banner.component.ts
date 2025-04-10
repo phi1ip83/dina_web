3750a54d2de1cdf66b849fdedcb0fb251f0e4351
phi1ip83 <philip.dossantos@hotmail.com>
2025-04-10 17:13:07 -0400

    Portrait fixed

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-banner',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
-
+  class1 = ['test1','test2'];
 }
