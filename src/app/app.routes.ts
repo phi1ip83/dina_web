5446f839c518c8230e7c32ad94cdebc98d367263
phi1ip83 <philip.dossantos@hotmail.com>
2025-04-11 10:43:14 -0400

    added contact page and fixed nav link for contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component:HomeComponent},
+    {path:'contact', component:ContactComponent}
 ];
