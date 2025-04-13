1fb5f41e18531397baed2790c2bf5e5a44fc5db8
phi1ip83 <philip.dossantos@hotmail.com>
2025-04-13 15:56:15 -0400

    contacts done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component:HomeComponent},
-    {path:'contact', component:ContactComponent}
+    {path:'contact', component:ContactComponent},
+    {path:'about', component:AboutComponent}
 ];
